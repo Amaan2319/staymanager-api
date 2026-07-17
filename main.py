bd44608564500cc83b84a067dcacf0bf9f1b1b8b
Amaan <amanabbassaiyed@gmail.com>
2026-07-17 07:58:59 +0530

    synchronous programming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import requests
+import time
 
 app = FastAPI()
 items = [{1: "Item 1"}]
@@ -9,6 +10,11 @@ class Item(BaseModel):
     name: int
     price: int
     is_available: bool
+
+class Dog(BaseModel):
+    name: str
+    breed: str
+    age: int
 
 @app.get("/")
 def root():
@@ -25,3 +31,21 @@ def get_item(item_id: int, q: str | None = None):
 @app.put("/items/{item_id}")
 def give_item(item_id:int, item: Item):
     return {"message": {"id": item_id, "name": item.name, "availability": item.is_available, "price": item.price}}
+
+@app.get("/dogs")
+def get_dogs():
+    request_start = time.time()
+    print(f"first request {request_start}")
+    result = requests.get('https://dogapi.dog/api/v1/facts?number=2').json()
+
+    print(f"Dogs: {result}")
+
+    return {'message': result}
+
+@app.get("/dogs/breeds")
+def get_breeds():
+    second_request = timme.time()
+    print(f"second request {second_request}")
+    breeds = requests.get('https://dogapi.dog/api/v2/breeds?page[size]=10').json()
+
+    return {"message": breeds}
