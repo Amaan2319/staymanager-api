69f724d79caea1868a6f92c3d7966794dd8381af
Amaan <amanabbassaiyed@gmail.com>
2026-07-24 15:29:17 +0530

    restarted fastapi

--- main.py
+++ main.py
@@ -1,57 +1,80 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import requests
+# import asyncio
+# import time
+
+# app = FastAPI()
+# items = [{1: "Item 1"}]
+
+# class Item(BaseModel):
+#     name: int
+#     price: int
+#     is_available: bool
+
+# class Dog(BaseModel):
+#     name: str
+#     breed: str
+#     age: int
+
+# @app.get("/")
+# def root():
+#     return {"hello": "world"}
+
+# @app.get("/items")
+# def get_items():
+#     return {"items": items}
+
+# @app.get("/item/{item_id}")
+# def get_item(item_id: int, q: str | None = None):
+#     return {"message": {item_id: q}}
+
+# @app.put("/items/{item_id}")
+# def give_item(item_id:int, item: Item):
+#     return {"message": {"id": item_id, "name": item.name, "availability": item.is_available, "price": item.price}}
+
+# @app.get("/dogs")
+# def get_dogs():
+#     request_start = time.time()
+#     print(f"first request {request_start}")
+#     result = requests.get('https://dogapi.dog/api/v1/facts?number=2').json()
+
+#     print(f"Dogs: {result}")
+
+#     return {'message': result}
+
+# @app.get("/dogs/breeds")
+# def get_breeds():
+#     second_request = time.time()
+#     print(f"second request {second_request}")
+#     breeds =  requests.get('https://dogapi.dog/api/v2/breeds?page[size]=10').json()
+
+#     return {"message": breeds}
+
+# @app.get("/async")
+# async def lateFunction():
+#     time.sleep(2)
+#     return {"message": "this is a forced asynchronous function"}
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests
-import asyncio
-import time
-
 app = FastAPI()
-items = [{1: "Item 1"}]
+
 
 class Item(BaseModel):
-    name: int
-    price: int
-    is_available: bool
-
-class Dog(BaseModel):
     name: str
-    breed: str
-    age: int
+    description: str
 
+items=[{"name": "Amaan", "description": "SD 1"}]
 @app.get("/")
 def root():
-    return {"hello": "world"}
+    return {"message": "Hello FastAPI"}
 
 @app.get("/items")
-def get_items():
-    return {"items": items}
+def getItems():
+    result = list(items)
+    return result
 
-@app.get("/item/{item_id}")
-def get_item(item_id: int, q: str | None = None):
-    return {"message": {item_id: q}}
-
-@app.put("/items/{item_id}")
-def give_item(item_id:int, item: Item):
-    return {"message": {"id": item_id, "name": item.name, "availability": item.is_available, "price": item.price}}
-
-@app.get("/dogs")
-def get_dogs():
-    request_start = time.time()
-    print(f"first request {request_start}")
-    result = requests.get('https://dogapi.dog/api/v1/facts?number=2').json()
-
-    print(f"Dogs: {result}")
-
-    return {'message': result}
-
-@app.get("/dogs/breeds")
-def get_breeds():
-    second_request = time.time()
-    print(f"second request {second_request}")
-    breeds =  requests.get('https://dogapi.dog/api/v2/breeds?page[size]=10').json()
-
-    return {"message": breeds}
-
-@app.get("/async")
-async def lateFunction():
-    time.sleep(2)
-    return {"message": "this is a forced asynchronous function"}
+@app.get("/items/{id}")
+def addItem(id: int):
+    return {"item_id": id}
