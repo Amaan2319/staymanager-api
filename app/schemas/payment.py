f1bced5cef26b9a00020e1a43d4d7b7e34f5deaa
Amaan2319 <109417887+Amaan2319@users.noreply.github.com>
2026-07-27 08:55:33 +0530

    Field in pydantic

--- app/schemas/payment.py
+++ app/schemas/payment.py
@@ -8,6 +8,6 @@ class PaymentStatus(Enum):
     FAILED="failed"
 
 class Payment(BaseModel):
-    id: int
+    id: int = Field(...,gt=0,description="The unique ID must be greater than 0")
     state: PaymentStatus
     amount: float=Field(...,gt=0.0,description="Payment cannot be negative or zero")
